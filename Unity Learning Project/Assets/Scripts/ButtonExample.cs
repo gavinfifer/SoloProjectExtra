20a83ddafba90dffcbf7bc967a3dd4705c8134f9
Gavin Fifer <gavin.fifer@digipen.edu>
2021-10-08 14:14:04 -0700

    Resolution Buttons
    Created a few buttons for testing that change the game resolution when clicked

---
+++ Unity Learning Project/Assets/Scripts/ButtonExample.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonExample : MonoBehaviour
+{
+    public void CloseGame()
+    {
+        Debug.Log("close game");
+    }
+}
